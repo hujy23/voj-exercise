867ee86b230f95e02202c3a62ab6af8ae89d2672
Era Hu <1020276595@qq.com>
2018-03-22 10:14:07 +0800

    problem 1001 compare top.v correct the Code indentation

--- problem-1001-compare/top.v
+++ problem-1001-compare/top.v
@@ -7,8 +7,8 @@ module compare(
 input   a, b;
 output  equal;
 
-localparam	EQUAL = 1'b1;
-localparam	NOT_EQUAL = 1'b0;
+localparam  EQUAL = 1'b1;
+localparam  NOT_EQUAL = 1'b0;
 
 assign  equal = (a == b) ? EQUAL : NOT_EQUAL;
 
